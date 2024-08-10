f5ccd6eba88141e984315cece74add531d9b813b
bernardo <bernardodemsj@gmail.com>
2024-08-10 15:01:52 -0300

    Adds mainActivity first screen

--- app/src/main/java/com/bernardomjunior/openlingo/ui/MainActivityScreen.kt
+++ app/src/main/java/com/bernardomjunior/openlingo/ui/MainActivityScreen.kt
@@ -1,25 +1,40 @@
 package com.bernardomjunior.openlingo.ui
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bernardomjunior.openlingo.components.TextButton
 import com.bernardomjunior.openlingo.ui.theme.OpenLingoTheme
 
 @Composable
 internal fun MainActivityScreen(
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.SpaceBetween
+    Surface(
+        modifier = modifier.fillMaxSize(),
     ) {
-
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            TextButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
+                text = "Exemplo",
+                onClick = {},
+                isLoading = true
+            )
+        }
     }
 }
 
@@ -28,8 +43,6 @@ internal fun MainActivityScreen(
 @Composable
 private fun ActivityScreenPreview() {
     OpenLingoTheme {
-        Surface {
-            MainActivityScreen()
-        }
+        MainActivityScreen()
     }
 }
